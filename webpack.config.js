3858739391e7535abca6f2e9ce76f5dae0787e60
miamam1 <theshpeecrab@gmail.com>
2024-08-21 18:53:58 +0100

    Added some div classes but nothing substantial yet.

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,21 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        use: {
+          loader: 'url-loader',
+        },
+    },
     ],
     }
 };
